972bc453c95259371928d12971c204330d1b4499
Vodjanoj <vladimirs.vodjanojs@gmail.com>
2022-05-03 20:50:19 +0300

    Adding Initial Routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { Route, Switch } from "react-router-dom";
+import AllQuotes from "./Pages/AllQuotes";
+import QuoteDetail from "./Pages/QuoteDetail";
+import NewQuote from "./Pages/NewQuote";
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route path="/quotes" exact>
+        <AllQuotes />
+      </Route>
+      {/* for rendering a detailed quote */}
+      <Route path="/quotes/:quoteId">
+        <QuoteDetail />
+      </Route>
+      <Route path="/new-quote">
+        <NewQuote />
+      </Route>
+    </Switch>
   );
 }
 
 export default App;
+
+
