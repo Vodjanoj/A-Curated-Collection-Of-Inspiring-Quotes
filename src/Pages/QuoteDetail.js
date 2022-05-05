e78016610767bf4b1c714701d444f916aa7abc21
Vodjanoj <vladimirs.vodjanojs@gmail.com>
2022-05-05 12:38:06 +0300

    Outputting Data on the "Details" Page

--- src/Pages/QuoteDetail.js
+++ src/Pages/QuoteDetail.js
@@ -1,12 +1,26 @@
 import { useParams, Route } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "Maximilian", text: "Learning React is great!" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  
+  // we might wanna check if quote is undefined, so if not quote, if we didn't find a quote
+  // for the ID we had in the URL, because of course a user can enter anything in that URL
+
+  if (!quote) {
+    return <p>No quote found!</p>
+  }
   return (
     <section>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       {/* can be used also a path path="/quotes/:quoteId/comments" */}
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
